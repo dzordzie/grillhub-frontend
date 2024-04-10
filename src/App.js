b97429a509d861cfe33658428bc29ef08481d03a
Juraj Valášek <dzordzie0@gmail.com>
2024-04-10 13:00:19 +0200

    refactor App.js with HomePage and MealPage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import SideBar from './components/SideBar'
 import NotFoundPage from './pages/NotFoundPage'
-// import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import LogInPage from './pages/LogInPage'
 import ProfilePage from './pages/ProfilePage'
@@ -16,10 +16,11 @@ function App() {
       <main className='sub-grid-container'>
         <Routes>
           <Route path='*' element={<NotFoundPage />} />
-          <Route path='/' element={<MealPage />} />
+          <Route path='/' element={<HomePage />} />
           <Route path='/registration' element={<RegisterPage />} />
           <Route path='/login' element={<LogInPage />} />
           <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/meal/:id' element={<MealPage />} />
         </Routes>
       </main>
     </div>
